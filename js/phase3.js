b66b52c43e1611fa10e04f7b83eeb3c5e9a06ab0
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-05-12 18:05:34 +0100

    completed phase3.js

---
+++ js/phase3.js
@@ -0,0 +1,29 @@
+const currentPage = 2;
+
+const fielImage = document.querySelector("#image");
+const fieldHobbie = document.querySelector("#hobbie");
+const forma = document.querySelector("form"); //CHANGE ID IN HTML! Change structure from div to form!!!
+
+//fill fields of the form with stored values
+fieldImage.value = readStorage("image");
+fieldStreet.value = readStorage("hobbie");
+
+protectContent();
+
+//variables to interact with validation and local storage
+let image = null;
+let hobbie = null;
+
+forma.addEventListener("submit", function(evnt){
+    evnt.preventDefault();
+    image = fieldImage.value;
+    hobbie = fieldHobbie.value;
+
+    if (imageVal(image)){
+        writeStorage("image");
+    }
+    if (hobbieVal(hobbie)){
+        writeStorage("hobbie")
+    }
+    accessTo3()
+})
