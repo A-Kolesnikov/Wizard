0f77d15a6fd4667cdfafe80e63ae3931fe49b1aa
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-05-14 13:51:32 +0300

    fixed phase3 - photo upload

--- js/phase3.js
+++ js/phase3.js
@@ -2,21 +2,22 @@ const currentPage = 3;
 
 const fieldImage = document.querySelector("#image");
 const fieldHobby = document.querySelector("#hobbies");
-const forma = document.querySelector("form"); //CHANGE ID IN HTML! Change structure from div to form!!!
+const forma = document.querySelector("form");
+const preview = document.querySelector("#preview")
 
 //fill fields of the form with stored values
-fieldImage.value = readStorage("image");
+fieldImage.value = '';
 fieldHobby.value = readStorage("hobbies");
 
 protectContent();
 
 //variables to interact with validation and local storage
-let image = "";
+let image = null;
 let hobbies = null;
 
 forma.addEventListener("submit", function(evnt){
     evnt.preventDefault();
-    image = fieldImage.files[0].name;
+    image = fieldImage.value;
     hobbies = fieldHobby.value;
 
     /*if (imageVal(image)){
@@ -26,3 +27,26 @@ forma.addEventListener("submit", function(evnt){
     writeStorage("hobbies")
     accessToSum()
 })
+
+function drawImage(){
+    image = readStorage("image")
+    if (image){
+        const prevImage = document.createElement("img");
+        prevImage.setAttribute('src', image)
+        preview.appendChild(prevImage);
+    }
+}
+
+function upload(input){
+    let file = input.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = function(){
+        image = reader.result;
+        writeStorage("image")
+        drawImage();
+    }
+}
+
+drawImage();
